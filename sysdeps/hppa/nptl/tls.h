d4f63cea3f1de267729b59b28e6fde67270be8f3
Gary Benson <gbenson@redhat.com>
2015-10-20 15:10:19 +0100

    Add Infinity THREAD_SELF macros to sysdeps/hppa/nptl/tls.h
    This commit adds Infinity THREAD_SELF macros for hppa.
    
    It's possible for this platform to put the Infinity macros before
    libthread_db's DB_THREAD_SELF, and define DB_THREAD_SELF in terms
    of the Infinity values, but I wasn't sure if it would be appropriate.

--- sysdeps/hppa/nptl/tls.h
+++ sysdeps/hppa/nptl/tls.h
@@ -117,6 +117,12 @@ typedef struct
 # define DB_THREAD_SELF \
   REGISTER (32, 32, 53 * 4, -sizeof (struct pthread))
 
+/* Magic for Infinity to know how to do THREAD_SELF.  */
+# define I8_THREAD_SELF I8_TS_REGISTER
+# define I8_TS_REG_SIZE 32
+# define I8_TS_REG_OFFSET 53 * 4
+# define I8_TS_REG_BIAS -sizeof (struct pthread)
+
 /* Access to data in the thread descriptor is easy.  */
 # define THREAD_GETMEM(descr, member) \
   descr->member
