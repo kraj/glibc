f2b84816d56e7f623cc72eba81e68600de093e9f
Khem Raj <raj.khem@gmail.com>
2015-12-31 14:35:35 -0800

    nativesdk-glibc: Allow 64 bit atomics for x86
    The fix consist of allowing 64bit atomic ops for x86.
    This should be safe for i586 and newer CPUs.
    It also makes the synchronization more efficient.
    
    Upstream-Status: Inappropriate [OE-Specific]
    
    Signed-off-by: Juro Bystricky <juro.bystricky@intel.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- sysdeps/x86/atomic-machine.h
+++ sysdeps/x86/atomic-machine.h
@@ -21,15 +21,7 @@
 
 #define USE_ATOMIC_COMPILER_BUILTINS	1
 
-#ifdef __x86_64__
-# define __HAVE_64B_ATOMICS		1
-#else
-/* Since the Pentium, i386 CPUs have supported 64-bit atomics, but the
-   i386 psABI supplement provides only 4-byte alignment for uint64_t
-   inside structs, so it is currently not possible to use 64-bit
-   atomics on this platform.  */
-# define __HAVE_64B_ATOMICS		0
-#endif
+#define __HAVE_64B_ATOMICS		1
 
 #define ATOMIC_EXCHANGE_USES_CAS	0
 
