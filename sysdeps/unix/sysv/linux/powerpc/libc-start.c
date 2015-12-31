a833fcd306d8bb4089a89c9d2106024478d2158a
Khem Raj <raj.khem@gmail.com>
2015-12-31 15:15:09 -0800

    eglibc: Clear cache lines on ppc8xx
    2007-06-13  Nathan Sidwell  <nathan@codesourcery.com>
                Mark Shinwell  <shinwell@codesourcery.com>
    
            * sysdeps/unix/sysv/linux/powerpc/libc-start.c
            (__libc_start_main): Detect 8xx parts and clear
            __cache_line_size if detected.
            * sysdeps/unix/sysv/linux/powerpc/dl-sysdep.c
            (DL_PLATFORM_AUXV): Likewise.
    
    Upstream-Status: Pending
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- sysdeps/unix/sysv/linux/powerpc/libc-start.c
+++ sysdeps/unix/sysv/linux/powerpc/libc-start.c
@@ -73,11 +73,25 @@ __libc_start_main (int argc, char **argv,
 
   /* Initialize the __cache_line_size variable from the aux vector.  For the
      static case, we also need _dl_hwcap, _dl_hwcap2 and _dl_platform, so we
-     can call __tcb_parse_hwcap_and_convert_at_platform ().  */
+     can call __tcb_parse_hwcap_and_convert_at_platform ().
+
+     This is used by memset to optimize setting to zero.  We have to
+     detect 8xx processors, which have buggy dcbz implementations that
+     cannot report page faults correctly.  That requires reading SPR,
+     which is a privileged operation.  Fortunately 2.2.18 and later
+     emulates PowerPC mfspr reads from the PVR register.  */
   for (ElfW (auxv_t) * av = auxvec; av->a_type != AT_NULL; ++av)
     switch (av->a_type)
       {
       case AT_DCACHEBSIZE:
+	if (__LINUX_KERNEL_VERSION >= 0x020218)
+	  {
+	    unsigned pvr = 0;
+
+	    asm ("mfspr %0, 287" : "=r" (pvr) :);
+	    if ((pvr & 0xffff0000) == 0x00500000)
+	      break;
+	  }
 	__cache_line_size = av->a_un.a_val;
 	break;
 #ifndef SHARED
