58dc3371865ef8e331de33423c57af855f6b6c45
Zack Weinberg <zackw@panix.com>
2016-07-07 17:10:02 -0300

    Deprecate inclusion of <sys/sysmacros.h> by <sys/types.h>
    The macros defined by <sys/sysmacros.h> are not part of POSIX nor XSI,
    and their names have been found frequently to collide with user code;
    see for instance glibc bug 19239 and Red Hat bug 130601.  <stdlib.h>
    includes <sys/types.h> under _GNU_SOURCE, and C++ code presently cannot
    avoid being compiled under _GNU_SOURCE, exacerbating the problem.
    
    	* NEWS: Inclusion of <sys/sysmacros.h> by <sys/types.h> is deprecated.
    	* misc/sys/sysmacros.h: If __SYSMACROS_DEPRECATED_INCLUSION is defined,
    	define major, minor, and makedev to issue deprecation warnings on use.
    	If __SYSMACROS_DEPRECATED_INCLUSION is *not* defined, suppress
    	previously-activated deprecation warnings for these macros and prevent
    	subsequent inclusions of this header from having any effect.
    	* posix/sys/types.h: Define __SYSMACROS_DEPRECATED_INCLUSION before
    	including <sys/sysmacros.h>, and undefine it again afterward.

--- misc/sys/sysmacros.h
+++ misc/sys/sysmacros.h
@@ -16,25 +16,59 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-#ifndef _SYS_SYSMACROS_H
-#define _SYS_SYSMACROS_H 1
+#ifndef _SYS_SYSMACROS_H_OUTER
 
-#include <features.h>
-#include <bits/types.h>
-#include <bits/sysmacros.h>
-
-#define __SYSMACROS_DECL(rtype, name, proto)                            \
-  extern rtype gnu_dev_##name proto __THROW __attribute_const__;
-
-#ifdef __USE_EXTERN_INLINES
-# define __SYSMACROS_IMPL(rtype, name, proto, body)             \
-  __SYSMACROS_DECL (rtype, name, proto)                         \
-  __extension__ __extern_inline __attribute_const__ rtype       \
-  __NTH (gnu_dev_##name proto) { body }
-#else
-# define __SYSMACROS_IMPL(rtype, name, proto, expr)    \
-  __SYSMACROS_DECL (rtype, name, proto)
+#ifndef __SYSMACROS_DEPRECATED_INCLUSION
+# define _SYS_SYSMACROS_H_OUTER 1
 #endif
+
+/* If <sys/sysmacros.h> is included after <sys/types.h>, these macros
+   will already be defined, and we need to redefine them without the
+   deprecation warnings.  (If they are included in the opposite order,
+   the outer #ifndef will suppress this entire file and the macros
+   will be usable without warnings.)  */
+#undef major
+#undef minor
+#undef makedev
+
+/* This is the macro that must be defined to satisfy the misuse check
+   in bits/sysmacros.h. */
+#ifndef _SYS_SYSMACROS_H
+# define _SYS_SYSMACROS_H 1
+
+# include <features.h>
+# include <bits/types.h>
+# include <bits/sysmacros.h>
+
+/* The extra "\n " moves gcc's [-Wdeprecated-declarations] annotation
+   onto the next line.  */
+# define __SYSMACROS_DEPRECATION_MSG(symbol)				     \
+  "\n  In the GNU C Library, `" #symbol "' is defined by <sys/sysmacros.h>." \
+  "\n  For historical compatibility, it is currently defined by"	     \
+  "\n  <sys/types.h> as well, but we plan to remove this soon."		     \
+  "\n  To use `" #symbol "', include <sys/sysmacros.h> directly."	     \
+  "\n  If you did not intend to use a system-defined macro `" #symbol "',"   \
+  "\n  you should #undef it after including <sys/types.h>."		     \
+  "\n "
+
+# define __SYSMACROS_DECL(rtype, name, proto)				     \
+   extern rtype gnu_dev_##name proto __THROW __attribute_const__;	     \
+   extern rtype __REDIRECT_NTH (__##name##_from_sys_types, proto,	     \
+				gnu_dev_##name)				     \
+    __attribute_const__							     \
+    __attribute_deprecated_msg__ (__SYSMACROS_DEPRECATION_MSG (name));
+
+# ifdef __USE_EXTERN_INLINES
+#  define __SYSMACROS_IMPL(rtype, name, proto, body)			     \
+    __SYSMACROS_DECL (rtype, name, proto)				     \
+    __extension__ __extern_inline __attribute_const__ rtype		     \
+    __NTH (gnu_dev_##name proto) { body }				     \
+    __extension__ __extern_inline __attribute_const__ rtype		     \
+    __NTH (__##name##_from_sys_types proto) { body }
+# else
+#  define __SYSMACROS_IMPL(rtype, name, proto, expr)			     \
+    __SYSMACROS_DECL (rtype, name, proto)
+# endif
 
 __BEGIN_DECLS
 
@@ -46,11 +80,19 @@ __SYSMACROS_IMPL (__dev_t, makedev,
 
 __END_DECLS
 
-#undef __SYSMACROS_IMPL
-#undef __SYSMACROS_DECL
+# undef __SYSMACROS_IMPL
+# undef __SYSMACROS_DECL
 
-#define major(dev) gnu_dev_major (dev)
-#define minor(dev) gnu_dev_minor (dev)
-#define makedev(maj, min) gnu_dev_makedev (maj, min)
+# endif /* _SYS_SYSMACROS_H */
+
+#ifdef __SYSMACROS_DEPRECATED_INCLUSION
+# define major(dev) __major_from_sys_types (dev)
+# define minor(dev) __minor_from_sys_types (dev)
+# define makedev(maj, min) __makedev_from_sys_types (maj, min)
+#else
+# define major(dev) gnu_dev_major (dev)
+# define minor(dev) gnu_dev_minor (dev)
+# define makedev(maj, min) gnu_dev_makedev (maj, min)
+#endif
 
 #endif /* sys/sysmacros.h */
