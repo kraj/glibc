883d2525fca68e86b68ca61300c1d18461e4596e
Albert ARIBAUD (3ADEV) <albert.aribaud@3adev.fr>
2017-05-24 10:27:17 +0200

    Y2038: implement Y2038-ready fstatat64, fxstatat (WIP)
    WIP: there is no Y2038-proof linux struct stat for now,
    so these implementations just use the existing syscalls
    and convert from kernel 32-bit-time struct stat64 to
    GLIBC Y2038-ready struct stat64

--- io/sys/stat.h
+++ io/sys/stat.h
@@ -443,6 +443,10 @@ extern int __REDIRECT_NTH (__xstat, (int __ver, const char *__filename,
 extern int __REDIRECT_NTH (__lxstat, (int __ver, const char *__filename,
 				      struct stat *__stat_buf), __lxstat64_t64)
      __nonnull ((2, 3));
+extern int __REDIRECT_NTH (__fxstatat, (int __ver, int __fildes,
+					const char *__filename,
+					struct stat *__stat_buf, int __flag),
+			   __fxstatat64_t64) __nonnull ((3, 4));
 #  else
 extern int __REDIRECT_NTH (__fxstat, (int __ver, int __fildes,
 				      struct stat *__stat_buf), __fxstat64)
@@ -453,11 +457,11 @@ extern int __REDIRECT_NTH (__xstat, (int __ver, const char *__filename,
 extern int __REDIRECT_NTH (__lxstat, (int __ver, const char *__filename,
 				      struct stat *__stat_buf), __lxstat64)
      __nonnull ((2, 3));
-#  endif
 extern int __REDIRECT_NTH (__fxstatat, (int __ver, int __fildes,
 					const char *__filename,
 					struct stat *__stat_buf, int __flag),
 			   __fxstatat64) __nonnull ((3, 4));
+#  endif
 
 # else
 #  ifdef __USE_TIME_BITS64
@@ -487,6 +491,9 @@ extern int __lxstat64_t64 (int __ver, const char *__filename,
 		       struct __stat64_t64 *__stat_buf) __THROW __nonnull ((2, 3));
 extern int __fxstatat64 (int __ver, int __fildes, const char *__filename,
 			 struct stat64 *__stat_buf, int __flag)
+     __THROW __nonnull ((3, 4));
+extern int __fxstatat64_t64 (int __ver, int __fildes, const char *__filename,
+			 struct __stat64_t64 *__stat_buf, int __flag)
      __THROW __nonnull ((3, 4));
 #endif
 extern int __xmknod (int __ver, const char *__path, __mode_t __mode,
