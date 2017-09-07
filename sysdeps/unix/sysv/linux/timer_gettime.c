d62aeaeb5960d39ea08a5298f85a219559793e37
Albert ARIBAUD (3ADEV) <albert.aribaud@3adev.fr>
2017-09-08 00:41:46 +0200

    Y2038: add function __timer_gettime64
    For Linux this uses the 32-bit time syscall, so it
    converts the syscall output into 64-bit time.

--- sysdeps/unix/sysv/linux/timer_gettime.c
+++ sysdeps/unix/sysv/linux/timer_gettime.c
@@ -39,3 +39,25 @@ timer_gettime (timer_t timerid, struct itimerspec *value)
 
   return res;
 }
+
+/* 64-bit time version */
+
+int
+__timer_gettime64 (timer_t timerid, struct __itimerspec64 *value)
+{
+  struct itimerspec value32;
+  struct timer *kt = (struct timer *) timerid;
+  int res;
+
+  res = INLINE_SYSCALL (timer_gettime, 2, kt->ktimerid, &value32);
+
+  if (res == 0)
+    {
+      value->it_value.tv_sec = value32.it_value.tv_sec;
+      value->it_value.tv_nsec = value32.it_value.tv_nsec;
+      value->it_interval.tv_sec = value32.it_interval.tv_sec;
+      value->it_interval.tv_nsec = value32.it_interval.tv_nsec;
+    }
+
+  return res;
+}
