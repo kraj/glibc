f4d2d74d440f1ec592b5f91eb7529254ed86dfc7
Albert ARIBAUD (3ADEV) <albert.aribaud@3adev.fr>
2017-09-08 00:41:37 +0200

    Y2038: add function __clock_nanosleep64

--- sysdeps/unix/sysv/linux/clock_nanosleep.c
+++ sysdeps/unix/sysv/linux/clock_nanosleep.c
@@ -21,7 +21,6 @@
 #include <sysdep-cancel.h>
 #include "kernel-posix-cpu-timers.h"
 
-
 /* We can simply use the syscall.  The CPU clocks are not supported
    with this function.  */
 int
@@ -52,3 +51,44 @@ __clock_nanosleep (clockid_t clock_id, int flags, const struct timespec *req,
 	  ? INTERNAL_SYSCALL_ERRNO (r, err) : 0);
 }
 weak_alias (__clock_nanosleep, clock_nanosleep)
+
+/* 64-bit time version */
+
+extern int __y2038_linux_support;
+
+int
+__clock_nanosleep64 (clockid_t clock_id, int flags,
+		   const struct __timespec64 *req,
+                   struct __timespec64 *rem)
+{
+  /* Until Linux provides a 64-bit syscall, use the 32-bit one */
+  INTERNAL_SYSCALL_DECL (err);
+  int r;
+  struct timespec req32, rem32, *rem32p = NULL;
+
+  if (req == NULL)
+    {
+      __set_errno(EFAULT);
+      return -1;
+    }
+
+  if (req->tv_sec > INT32_MAX || req->tv_sec < INT32_MIN)
+    {
+      __set_errno(EOVERFLOW);
+      return -1;
+    }
+
+  req32.tv_sec = req->tv_sec;
+  req32.tv_nsec = req->tv_nsec;
+  if (rem !=NULL) rem32p = &rem32;
+
+  r = clock_nanosleep(clock_id, flags, &req32, rem32p);
+
+  if (r == 0 && rem != NULL)
+    {
+      rem->tv_sec = rem32.tv_sec;
+      rem->tv_nsec = rem32.tv_nsec;
+    }
+
+  return r;
+}
