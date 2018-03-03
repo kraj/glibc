6efff650203b78a2913c9e3dad126c5630ff84ac
Zack Weinberg <zackw@panix.com>
2018-03-03 08:00:50 -0500

    Use STRFMON_LDBL_IS_DBL instead of __ldbl_is_dbl.
    This patch takes the first step toward removing the global flag
    __ldbl_is_dbl, creating a __vstrfmon_l_internal that takes a flags
    parameter instead.
    
    This change arguably makes the generated code slightly worse on
    architectures where __ldbl_is_dbl is never true; right now, on those
    architectures, it's a compile-time constant; after this change, the
    compiler could theoretically prove that __vstrfmon_l_internal was
    never called with a nonzero flags argument, but it would probably need
    LTO to do it.  This is not performance critical code and I tend to
    think that the maintainability benefits of removing action at a
    distance are worth it.  However, we _could_ wrap the runtime flag
    check with a macro that was defined to ignore its argument and always
    return false on architectures where __ldbl_is_dbl is never true, if
    people think the codegen benefits are important.
    
    	* include/monetary.h (STRFMON_LDBL_IS_DBL): New constant.
    	(__vstrfmon_l): Rename to __vstrfmon_l_internal and add flags
    	argument.
    	* stdlib/strfmon_l.c (__vstrfmon_l): Rename to __vstrfmon_l_internal
    	and add flags argument.	 Check flags instead of __ldbl_is_dbl when
    	deciding whether to set is_long_double.
    	(__strfmon_l): Call __vstrfmon_l_internal instead of __vstrfmon_l,
    	passing zero for flags argument.
    	* stdlib/strfmon.c (strfmon): Same change as made to __strfmon_l.
    
    	* sysdeps/ieee754/ldbl-opt/nldbl-compat.c
    	(__nldbl___vstrfmon, __nldbl___vstrfmon_l)
    	(__nldbl_strfmon, __nldbl___strfmon_l): Call __vstrfmon_l_internal
    	directly, passing STRFMON_LDBL_IS_DBL for flags argument.  Normalize
    	variable names.  Remove libc_hidden_def/libc_hidden_proto.
    	* sysdeps/ieee754/ldbl-opt/nldbl-compat.h: Don't use NLDBL_DECL
    	for __nldbl___vstrfmon_l.
    
    	* manual/locale.texi: Update a reference to vstrfmon_l in comments.

--- include/monetary.h
+++ include/monetary.h
@@ -2,7 +2,11 @@
 #ifndef _ISOMAC
 #include <stdarg.h>
 
-extern ssize_t __vstrfmon_l (char *s, size_t maxsize, locale_t loc,
-			     const char *format, va_list ap)
-     attribute_hidden;
+extern ssize_t __vstrfmon_l_internal (char *s, size_t maxsize, locale_t loc,
+                                      const char *format, va_list ap,
+                                      unsigned int flags);
+
+/* Flags for __vstrfmon_l_internal.  */
+#define STRFMON_LDBL_IS_DBL 0x0001
+
 #endif
