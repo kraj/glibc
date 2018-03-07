665a5665924b44f5de0938e4c1a077d5bcfee061
Zack Weinberg <zackw@panix.com>
2018-03-07 09:03:26 -0500

    Add __vsyslog_internal, with same flags as __v*printf_internal.
    __nldbl___vsyslog_chk will ultimately want to pass PRINTF_LDBL_IS_DBL
    down to __vfprintf_internal *as well as* possibly setting PRINTF_FORTIFY.
    To make that possible, we need a __vsyslog_internal that takes the
    same flags as printf.  The code in misc/syslog.c does also get a
    little simpler.
    
    	* misc/syslog.c: Include libioP.h, not iolibio.h.
    	(__vsyslog_internal): New function with the former body of
    	__vsyslog_chk; takes mode_flags argument same as
    	__v*printf_internal.  Call __vfprintf_internal directly.
    
    	(__vsyslog_chk): Now a wrapper around __vsyslog_internal.
    	Remove libc_hidden_def.
    	(__syslog, __syslog_chk): Use __vsyslog_internal.
    	(__vsyslog): Move to just below __syslog.  Use __vsyslog_internal.
    
    	* include/sys/syslog.h: Add multiple inclusion guard.
    	Add prototype for __vsyslog_internal.
    	Remove libc_hidden_proto for __vsyslog_chk.
    
    	* sysdeps/ieee754/ldbl-opt/nldbl-compat.c (__nldbl___vsyslog_chk):
    	Use __vsyslog_internal.

--- include/sys/syslog.h
+++ include/sys/syslog.h
@@ -1,11 +1,20 @@
+#ifndef _LIBC_SYS_SYSLOG_H
+#define _LIBC_SYS_SYSLOG_H 1
 #include <misc/sys/syslog.h>
-
 #ifndef _ISOMAC
+
 libc_hidden_proto (syslog)
 libc_hidden_proto (vsyslog)
+
+/* __vsyslog_internal uses the same mode_flags bits as
+   __v*printf_internal; see libio/libioP.h.  */
+extern void __vsyslog_internal (int pri, const char *fmt, __gnuc_va_list ap,
+                                unsigned int mode_flags)
+     __attribute__ ((__format__ (__printf__, 2, 0)));
 
 extern void __vsyslog_chk (int __pri, int __flag, const char *__fmt,
 			   __gnuc_va_list __ap)
      __attribute__ ((__format__ (__printf__, 3, 0)));
-libc_hidden_proto (__vsyslog_chk)
-#endif
+
+#endif /* _ISOMAC */
+#endif /* syslog.h */
