7e471b62b77ee965d26c19af199b0b5ec8c759ea
Khem Raj <raj.khem@gmail.com>
2020-05-15 17:05:45 -0700

    wordsize.h: Unify the header between arm and aarch64
    This helps OE multilibs to not sythesize this header which causes all
    kind of recursions and other issues since wordsize is fundamental header
    and ends up including itself in many case e.g. clang tidy, bpf etc.
    
    Upstream-Status: Inappropriate [ OE-Specific ]
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- sysdeps/aarch64/bits/wordsize.h
+++ sysdeps/aarch64/bits/wordsize.h
@@ -17,12 +17,19 @@
    License along with the GNU C Library; if not, see
    <https://www.gnu.org/licenses/>.  */
 
-#ifdef __LP64__
+#if defined (__aarch64__) && defined (__LP64__)
 # define __WORDSIZE			64
-#else
+#elif defined (__aarch64__)
 # define __WORDSIZE			32
 # define __WORDSIZE32_SIZE_ULONG	1
 # define __WORDSIZE32_PTRDIFF_LONG	1
+#else
+# define __WORDSIZE			32
+# define __WORDSIZE_TIME64_COMPAT32	1
+# define __WORDSIZE32_SIZE_ULONG	0
+# define __WORDSIZE32_PTRDIFF_LONG	0
 #endif
 
+#ifdef __aarch64__
 #define __WORDSIZE_TIME64_COMPAT32	0
+#endif
