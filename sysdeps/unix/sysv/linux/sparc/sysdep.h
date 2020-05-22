a36f5eda4ac4ea6543b4f7dad4d6475acdac9793
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2020-05-22 10:59:03 -0300

    sparc: Add internal_syscall

--- sysdeps/unix/sysv/linux/sparc/sysdep.h
+++ sysdeps/unix/sysv/linux/sparc/sysdep.h
@@ -194,6 +194,164 @@
 	__o0;								\
 })
 
+
+#ifdef __arch64__
+# define __SYSCALL_TRAP_NUM     "0x6d"
+# define __SYSCALL_BCC_INST     "bcc,pt %%xcc, 1f"
+# define __SYSCALL_ASM_CLOBBERS						\
+	__SYSCALL_COMMON_CLOBBERS ,					\
+	"f32", "f34", "f36", "f38", "f40", "f42", "f44", "f46",		\
+	"f48", "f50", "f52", "f54", "f56", "f58", "f60", "f62"
+#else
+# define __SYSCALL_TRAP_NUM     "0x10"
+# define __SYSCALL_BCC_INST     "bcc    1f"
+# define __SYSCALL_ASM_CLOBBERS						\
+ 	__SYSCALL_COMMON_CLOBBERS
+#endif /* __arch64__  */
+#define __SYSCALL_COMMON_CLOBBERS					\
+	"f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7",			\
+	"f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15",		\
+	"f16", "f17", "f18", "f19", "f20", "f21", "f22", "f23",		\
+	"f24", "f25", "f26", "f27", "f28", "f29", "f30", "f31",		\
+	"cc", "memory"
+
+static inline long int
+__internal_syscall0 (long int name)
+{
+  register long int g1 asm ("g1") = name;
+  register long int o0 asm ("o0");
+  asm volatile ("ta " __SYSCALL_TRAP_NUM ";"
+		__SYSCALL_BCC_INST ";"
+		" nop;"
+		"sub  %%g0, %%o0, %%o0;"
+		"1:"
+		: "=r" (o0)
+		: "r" (g1)
+		: __SYSCALL_ASM_CLOBBERS);
+  return o0;
+}
+
+static inline long int
+__internal_syscall1 (long int name, __syscall_arg_t arg1)
+{
+  register long int g1 asm ("g1") = name;
+  register long int o0 asm ("o0") = arg1;
+  asm volatile ("ta " __SYSCALL_TRAP_NUM ";"
+		__SYSCALL_BCC_INST ";"
+		" nop;"
+		"sub  %%g0, %%o0, %%o0;"
+		"1:"
+		: "=r" (o0)
+		: "r" (g1), "0" (o0)
+		: __SYSCALL_ASM_CLOBBERS);
+  return o0;
+}
+
+static inline long int
+__internal_syscall2 (long int name, __syscall_arg_t arg1,
+		     __syscall_arg_t arg2)
+{
+  register long int g1 asm ("g1") = name;
+  register long int o0 asm ("o0") = arg1;
+  register long int o1 asm ("o1") = arg2;
+  asm volatile ("ta " __SYSCALL_TRAP_NUM ";"
+		__SYSCALL_BCC_INST ";"
+		" nop;"
+		"sub  %%g0, %%o0, %%o0;"
+		"1:"
+		: "=r" (o0)
+		: "r" (g1), "0" (o0), "r" (o1)
+		: __SYSCALL_ASM_CLOBBERS);
+  return o0;
+}
+
+static inline long int
+__internal_syscall3 (long int name, __syscall_arg_t arg1,
+		     __syscall_arg_t arg2, __syscall_arg_t arg3)
+{
+  register long int g1 asm ("g1") = name;
+  register long int o0 asm ("o0") = arg1;
+  register long int o1 asm ("o1") = arg2;
+  register long int o2 asm ("o2") = arg3;
+  asm volatile ("ta " __SYSCALL_TRAP_NUM ";"
+		__SYSCALL_BCC_INST ";"
+		" nop;"
+		"sub  %%g0, %%o0, %%o0;"
+		"1:"
+		: "=r" (o0)
+		: "r" (g1), "0" (o0), "r" (o1), "r" (o2)
+		: __SYSCALL_ASM_CLOBBERS);
+  return o0;
+}
+
+static inline long int
+__internal_syscall4 (long int name, __syscall_arg_t arg1,
+		     __syscall_arg_t arg2, __syscall_arg_t arg3,
+		     __syscall_arg_t arg4)
+{
+  register long int g1 asm ("g1") = name;
+  register long int o0 asm ("o0") = arg1;
+  register long int o1 asm ("o1") = arg2;
+  register long int o2 asm ("o2") = arg3;
+  register long int o3 asm ("o3") = arg4;
+  asm volatile ("ta " __SYSCALL_TRAP_NUM ";"
+		__SYSCALL_BCC_INST ";"
+		" nop;"
+		"sub  %%g0, %%o0, %%o0;"
+		"1:"
+		: "=r" (o0)
+		: "r" (g1), "0" (o0), "r" (o1), "r" (o2), "r" (o3)
+		: __SYSCALL_ASM_CLOBBERS);
+  return o0;
+}
+
+static inline long int
+__internal_syscall5 (long int name, __syscall_arg_t arg1,
+		     __syscall_arg_t arg2, __syscall_arg_t arg3,
+		     __syscall_arg_t arg4, __syscall_arg_t arg5)
+{
+  register long int g1 asm ("g1") = name;
+  register long int o0 asm ("o0") = arg1;
+  register long int o1 asm ("o1") = arg2;
+  register long int o2 asm ("o2") = arg3;
+  register long int o3 asm ("o3") = arg4;
+  register long int o4 asm ("o4") = arg5;
+  asm volatile ("ta " __SYSCALL_TRAP_NUM ";"
+		__SYSCALL_BCC_INST ";"
+		" nop;"
+		"sub  %%g0, %%o0, %%o0;"
+		"1:"
+		: "=r" (o0)
+		: "r" (g1), "0" (o0), "r" (o1), "r" (o2), "r" (o3), "r" (o4)
+		: __SYSCALL_ASM_CLOBBERS);
+  return o0;
+}
+
+static inline long int
+__internal_syscall6 (long int name, __syscall_arg_t arg1,
+		     __syscall_arg_t arg2, __syscall_arg_t arg3,
+		     __syscall_arg_t arg4, __syscall_arg_t arg5,
+		     __syscall_arg_t arg6)
+{
+  register long int g1 asm ("g1") = name;
+  register long int o0 asm ("o0") = arg1;
+  register long int o1 asm ("o1") = arg2;
+  register long int o2 asm ("o2") = arg3;
+  register long int o3 asm ("o3") = arg4;
+  register long int o4 asm ("o4") = arg5;
+  register long int o5 asm ("o5") = arg6;
+  asm volatile ("ta " __SYSCALL_TRAP_NUM ";"
+		__SYSCALL_BCC_INST ";"
+		" nop;"
+		"sub  %%g0, %%o0, %%o0;"
+		"1:"
+		: "=r" (o0)
+		: "r" (g1), "0" (o0), "r" (o1), "r" (o2), "r" (o3), "r" (o4),
+		  "r" (o5)
+		: __SYSCALL_ASM_CLOBBERS);
+  return o0;
+}
+
 #endif	/* __ASSEMBLER__ */
 
 #endif /* _LINUX_SPARC_SYSDEP_H */
