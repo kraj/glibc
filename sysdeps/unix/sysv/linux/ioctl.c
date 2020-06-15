37839b288f9bb8508ade4164479ae6b3e4e72fb9
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2020-06-15 13:57:54 -0300

    Linux: implement ioctl in C

---
+++ sysdeps/unix/sysv/linux/ioctl.c
@@ -0,0 +1,36 @@
+/* Control device.  Linux version.
+   Copyright (C) 2020 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <sys/ioctl.h>
+#include <stdarg.h>
+#include <sysdep.h>
+
+int
+__ioctl (int fd, unsigned long int request, ...)
+{
+  void *arg;
+  va_list va;
+
+  va_start (va, request);
+  arg = va_arg (va, void *);
+  va_end (va);
+
+  return inline_syscall (__NR_ioctl, fd, request, arg);
+}
+libc_hidden_def (__ioctl)
+weak_alias (__ioctl, ioctl)
