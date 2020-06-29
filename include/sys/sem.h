3ca7a464ef33a0ff1bb4e4b4003aa844027dc0c8
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2020-06-29 13:35:50 -0300

    sysv: linux: Add 64-bit time_t variant for semctl
    Different than others 64-bit time_t syscalls, the SysIPC interface
    does not provide a new set of syscall for y2038 safeness.  Instead it
    uses unused fields in semid_ds structure to return the high bits for
    the timestamps.
    
    To provide a y2038 safe interface a new symbol __semctl64 is added
    and __semctl is change to call it instead (it adds some extra buffer
    copy for the 32 bit time_t implementation).
    
    Two new structures are added:
    
      1. kernel_semid64_ds: used internally only on 32-bit architectures
         to issue the syscall.  A handful of architectures (hppa, i386,
         mips, powerpc32, sparc32) require specific implementations due
         their kernel ABI.
    
      2. semid_ds64: this is only for __TIMESIZE != 64 to use along with
         the 64-bit semctl.  It is different than the kernel struct because
         the exported 64-bit time_t might require different alignment
         depending of the architecture ABI.
    
    So the resulting implementation does:
    
      1. For 64-bit architectures it assumes semid_ds already contains
         64-bit time_t fields and will result in just the __semctl symbol
         using the __semctl64 code.  The semid_ds argument is passed as-is
         to the syscall.
    
      2. For 32-bit architectures with default 64-bit time_t (newer ABIs
         such riscv32 or arc), it will also result in only one exported
         symbol but with the required high/low handling.
    
         It might be possible to optimize it further to avoid the
         kernel_semid64_ds to semun transformation if the exported ABI
         for the architectures matches the expected kernel ABI, but the
         implementation is already complex enough and don't think this
         should be a hotspot in any case.
    
      3. Finally for 32-bit architecture with both 32-bit and 64-bit time_t
         support we follow the already set way to provide one symbol with
         64-bit time_t support and implement the 32-bit time_t support
         using the 64-bit one.
    
         The default 32-bit symbol will allocate and copy the semid_ds
         over multiple buffers, but this should be deprecated in favor
         of the __semctl64 anyway.
    
    Checked on i686-linux-gnu and x86_64-linux-gnu.  I also did some sniff
    tests on powerpc, powerpc64, mips, mips64, armhf, sparcv9, and
    sparc64.
    
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Tested-by: Vineet Gupta <vgupta@synopsys.com>
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>
    Tested-by: Carlos O'Donell <carlos@redhat.com>

--- include/sys/sem.h
+++ include/sys/sem.h
@@ -5,5 +5,12 @@
 
 __typeof__ (semtimedop) __semtimedop attribute_hidden;
 
+#if __TIMESIZE == 64
+# define __semctl64 __semctl
+#else
+extern int __semctl64 (int semid, int semnum, int cmd, ...);
+libc_hidden_proto (__semctl64);
+#endif
+
 # endif
 #endif
