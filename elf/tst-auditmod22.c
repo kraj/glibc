d3d1136de30ad744bd58fd1d2342316c29e5b8f2
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2021-07-19 18:42:26 -0300

    elf: Issue audit la_objopen() for vDSO
    The vDSO is is listed in the link_map chain, but is never the subject of
    an la_objopen call.  A new internal flag __RTLD_VDSO is added that
    acts as __RTLD_OPENEXEC to allocate the required 'struct auditstate'
    extra space for the 'struct link_map'.
    
    The return value from the callback() is currently ignored, since there
    is no PLT call involved by glibc when using the vDSO, neither the vDSO
    are exported directly.
    
    Checked on x86_64-linux-gnu, i686-linux-gnu, and aarch64-linux-gnu.

---
+++ elf/tst-auditmod22.c
@@ -0,0 +1,37 @@
+/* Check DTAUDIT and vDSO interaction.
+   Copyright (C) 2021 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <link.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <sys/auxv.h>
+
+unsigned int
+la_version (unsigned int version)
+{
+  return LAV_CURRENT;
+}
+
+unsigned int
+la_objopen (struct link_map *map, Lmid_t lmid, uintptr_t *cookie)
+{
+  if (map->l_addr == getauxval (AT_SYSINFO_EHDR))
+    fprintf (stderr, "vdso found: %" PRIxPTR "\n", (uintptr_t) map->l_addr);
+
+  return 0;
+}
