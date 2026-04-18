1a74f82eedaf0987eda1f522121e6a48fac502e7
Rocket Ma <marocketbd@gmail.com>
2026-04-17 23:48:41 -0700

    stdio-common: Fix buffer overflow in scanf %mc [BZ #34008]
    * stdio-common/vfscanf-internal.c: When enlarging allocated buffer with
    format %mc or %mC, glibc allocates one byte less, leading to
    user-controlled one byte overflow. This commit fixes BZ #34008, or
    CVE-2026-5450.
    
    Reviewed-by: Carlos O'Donell <carlos@redhat.com>
    Signed-off-by: Rocket Ma <marocketbd@gmail.com>
    Reviewed-by: H.J. Lu <hjl.tools@gmail.com>
    (cherry picked from commit 839898777226a3ed88c0859f25ffe712519b4ead)

--- stdio-common/vfscanf-internal.c
+++ stdio-common/vfscanf-internal.c
@@ -853,8 +853,7 @@ __vfscanf_internal (FILE *s, const char *format, va_list argptr,
 			{
 			  /* Enlarge the buffer.  */
 			  size_t newsize
-			    = strsize
-			      + (strsize >= width ? width - 1 : strsize);
+			    = strsize + (strsize >= width ? width : strsize);
 
 			  str = (char *) realloc (*strptr, newsize);
 			  if (str == NULL)
@@ -930,7 +929,7 @@ __vfscanf_internal (FILE *s, const char *format, va_list argptr,
 		      && wstr == (wchar_t *) *strptr + strsize)
 		    {
 		      size_t newsize
-			= strsize + (strsize > width ? width - 1 : strsize);
+			= strsize + (strsize >= width ? width : strsize);
 		      /* Enlarge the buffer.  */
 		      wstr = (wchar_t *) realloc (*strptr,
 						  newsize * sizeof (wchar_t));
@@ -985,7 +984,7 @@ __vfscanf_internal (FILE *s, const char *format, va_list argptr,
 		    && wstr == (wchar_t *) *strptr + strsize)
 		  {
 		    size_t newsize
-		      = strsize + (strsize > width ? width - 1 : strsize);
+		      = strsize + (strsize >= width ? width : strsize);
 		    /* Enlarge the buffer.  */
 		    wstr = (wchar_t *) realloc (*strptr,
 						newsize * sizeof (wchar_t));
