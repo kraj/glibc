f7623844a710fcd483a4d8fbc99a901977cf76b8
Uros Bizjak <ubizjak@gmail.com>
2026-05-06 16:35:54 +0200

    stdlib: Add internal stdc_rotate_left implementation
    It follows the C2y N3367 proposed interface, along with some tests
    imported from gnulib (and adapted to glibc libsupport).
    
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    Reviewed-by: Adhemerval Zanella  <adhemerval.zanella@linaro.org>

--- include/stdbit.h
+++ include/stdbit.h
@@ -5,6 +5,66 @@
 #ifndef _ISOMAC
 # include <stdint.h>
 
+#  if __glibc_has_builtin (__builtin_stdc_rotate_left)
+#   define stdc_rotate_left(__x, __n) (__builtin_stdc_rotate_left (__x, __n))
+#   define stdc_rotate_left_uc(__x, __n) (stdc_rotate_left (__x, __n))
+#   define stdc_rotate_left_us(__x, __n) (stdc_rotate_left (__x, __n))
+#   define stdc_rotate_left_ui(__x, __n) (stdc_rotate_left (__x, __n))
+#   define stdc_rotate_left_ul(__x, __n) (stdc_rotate_left (__x, __n))
+#   define stdc_rotate_left_ull(__x, __n) (stdc_rotate_left (__x, __n))
+#  else
+#   if __WORDSIZE == 64
+#    define __ROL_UL_GENERIC __rol64_inline
+#   else
+#    define __ROL_UL_GENERIC __rol32_inline
+#   endif
+#   define stdc_rotate_left(__x, __n)		\
+   _Generic((__x),				\
+      unsigned char: __rol8_inline,		\
+      unsigned short: __rol16_inline,		\
+      unsigned int: __rol32_inline,		\
+      unsigned long: __ROL_UL_GENERIC,		\
+      unsigned long long: __rol64_inline	\
+   )(__x, __n)
+
+#   define __rol_generic(__v, __n)		\
+ ((__v << (__n & (sizeof __v * 8 - 1)))		\
+  | (__v >> ((-__n) & (sizeof __v * 8 - 1))))
+
+static __always_inline uint8_t
+__rol8_inline (uint8_t __x, unsigned int __n)
+{
+  return __rol_generic (__x, __n);
+}
+
+static __always_inline uint16_t
+__rol16_inline (uint16_t __x, unsigned int __n)
+{
+  return __rol_generic (__x, __n);
+}
+
+static __always_inline uint32_t
+__rol32_inline (uint32_t __x, unsigned int __n)
+{
+  return __rol_generic (__x, __n);
+}
+
+static __always_inline uint64_t
+__rol64_inline (uint64_t __x, unsigned int __n)
+{
+  return __rol_generic (__x, __n);
+}
+#   define stdc_rotate_left_uc(__x, __n) (__rol8_inline (__x, __n))
+#   define stdc_rotate_left_us(__x, __n) (__rol16_inline (__x, __n))
+#   define stdc_rotate_left_ui(__x, __n) (__rol32_inline (__x, __n))
+#   if __WORDSIZE == 64
+#    define stdc_rotate_left_ul(__x, __n) (__rol64_inline (__x, __n))
+#   else
+#    define stdc_rotate_left_ul(__x, __n) (__rol32_inline (__x, __n))
+#   endif
+#   define stdc_rotate_left_ull(__x, __n) (__rol64_inline (__x, __n))
+#  endif /* __glibc_has_builtin (__builtin_stdc_rotate_left) */
+
 #  if __glibc_has_builtin (__builtin_stdc_rotate_right)
 #   define stdc_rotate_right(__x, __n) (__builtin_stdc_rotate_right (__x, __n))
 #   define stdc_rotate_right_uc(__x, __n) (stdc_rotate_right (__x, __n))
@@ -49,7 +109,7 @@ __ror32_inline (uint32_t __x, unsigned int __n)
   return __ror_generic (__x, __n);
 }
 
-static __always_inline  uint64_t
+static __always_inline uint64_t
 __ror64_inline (uint64_t __x, unsigned int __n)
 {
   return __ror_generic (__x, __n);
