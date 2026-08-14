7d26579873791748c20d874adb66e877412175dd
Wilco Dijkstra <wilco.dijkstra@arm.com>
2026-08-14 14:18:01 +0000

    malloc: Show hugetlb tunable default in --list-tunables
    Update the hugetlb tunable default in elf/dl-tunables.c so it is shown as 1
    with /lib/ld-linux-aarch64.so.1 --list-tunables.
    Move the intitialization of thp_mode/thp_pagesize to do_set_hugetlb() and
    avoid accessing /sys/kernel/mm if DEFAULT_THP_PAGESIZE > 0.  Switch off THP if
    glibc.malloc.hugetlb=0 is used - this behaves as if DEFAULT_THP_PAGESIZE==0.
    Fix the --list-tunables testcase.
    
    Reviewed-by: DJ Delorie <dj@redhat.com>
    Signed-off-by: Alexander Sverdlin <alexander.sverdlin@siemens.com>
    (cherry picked from commit e87c151130216f5c62f0e172e3eaebbc4a5d794a)

--- elf/dl-tunables.c
+++ elf/dl-tunables.c
@@ -33,6 +33,7 @@
 #include <array_length.h>
 #include <dl-minimal-malloc.h>
 #include <dl-symbol-redir-ifunc.h>
+#include <malloc-hugepages.h>
 
 #define TUNABLES_INTERNAL 1
 #include "dl-tunables.h"
@@ -295,6 +296,10 @@ __tunables_init (char **envp)
   char *envname = NULL;
   char *envval = NULL;
   char **prev_envp = envp;
+
+  /* Default to glibc.malloc.hugetlb=1 if DEFAULT_THP_PAGESIZE is non-zero.  */
+  if (DEFAULT_THP_PAGESIZE > 0)
+    TUNABLE_SET (glibc, malloc, hugetlb, 1);
 
   /* Ignore tunables for AT_SECURE programs.  */
   if (__libc_enable_secure)
--- malloc/malloc.c
+++ malloc/malloc.c
@@ -5114,10 +5114,26 @@ do_set_mxfast (size_t value)
 static __always_inline int
 do_set_hugetlb (size_t value)
 {
+  /* Enable THP if DEFAULT_THP_PAGESIZE is non-zero.  */
+  if (DEFAULT_THP_PAGESIZE > 0)
+    {
+      mp_.thp_mode = malloc_thp_mode_madvise;
+      mp_.thp_pagesize = DEFAULT_THP_PAGESIZE;
+    }
+
   if (value == 0)
-    mp_.thp_mode = malloc_thp_mode_never;
+    {
+      /* Turn off THP support completely.  */
+      mp_.thp_mode = malloc_thp_mode_never;
+      mp_.thp_pagesize = 0;
+    }
   else if (value == 1)
     {
+      /* Avoid querying the THP page size/mode since accessing /sys/kernel/mm
+        is relatively slow and might not be accessible in containers.  */
+      if (DEFAULT_THP_PAGESIZE > 0)
+       return 0;
+
       mp_.thp_mode = __malloc_thp_mode ();
       if (mp_.thp_mode == malloc_thp_mode_madvise
           || mp_.thp_mode == malloc_thp_mode_always)
