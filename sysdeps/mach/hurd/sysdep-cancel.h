81f3dfabc9ce9bc15178acbddfac559aefd6b8df
Samuel Thibault <samuel.thibault@ens-lyon.org>
2026-08-29 20:34:14 +0200

    hurd: Move SINGLE_THREAD_P / RTLD_SINGLE_THREAD_P to single-thread.h
    Like on Linux

--- sysdeps/mach/hurd/sysdep-cancel.h
+++ sysdeps/mach/hurd/sysdep-cancel.h
@@ -6,11 +6,6 @@ void __pthread_disable_asynccancel (int oldtype);
 #pragma weak __pthread_enable_asynccancel
 #pragma weak __pthread_disable_asynccancel
 
-/* Always multi-thread (since there's at least the sig handler), but no
-   handling enabled.  */
-#define SINGLE_THREAD_P (0)
-#define RTLD_SINGLE_THREAD_P (0)
-
 #define LIBC_CANCEL_ASYNC() ({ \
 	int __cancel_oldtype = 0; \
 	if (__pthread_enable_asynccancel) \
